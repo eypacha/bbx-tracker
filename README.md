6d9f3ce8d0ca5d835b2d1bf1009643d8b6252618
Ey Pacha! <contacto@eypacha.com.ar>
2017-05-19 03:28:21 -0300

    ultima version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# bbx-tracker
+# BBX Tracker!
 I'm a simple BeatBox Tracker hecho en JavasScript.
 
 [DEMO](https://eypacha.github.io/bbx-tracker/)
--- js/main.js
+++ js/main.js
@@ -8,9 +8,18 @@ var bpm = 80;
 var minBpm = 10;
 var maxBpm = 300;
 var maxSteps = 8;
+var images = new Array();
+
+function preload() {
+	for (i = 0; i < preload.arguments.length; i++) {
+		images[i] = new Image()
+		images[i].src = "img/pacha-boca-" + preload.arguments[i] + ".png";
+	}
+}
+preload("B-","Pf","ts","c1","c2","c3","^K","^m","BB");
 
 $(document).ready(function() {
-  
+	
 $("#playBtn").on('click', playTracker);
 $("#stopBtn").on('click', stopTracker);
 $("#loopBtn").on('click', toggleLoop);
@@ -20,6 +29,8 @@ $("#tempoUpUp").on('click', function(){changeTempo(+10)});
 $("#tempoDownDown").on('click', function(){changeTempo(-10)});
 $("#moreSteps").on('click', function(){changeSteps(+1)});
 $("#lessSteps").on('click', function(){changeSteps(-1)});
+$("#shareBtn").on('click', showShare);
+$("#shortlink").on( "click", function(){$("#shortlink").select();} );
 	
  sound = new Howl({
   src: ['audio/sounds.mp3'],
@@ -32,10 +43,22 @@ $("#lessSteps").on('click', function(){changeSteps(-1)});
     'c2': [2666, 300],
     'c3': [3373, 300],
     '^K': [2135, 250],
-    'mm': [3869, 250],
+    '^m': [3869, 250],
     'BB': [5201, 350]
   }
 });
+	
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var sharedBeat = getParameterByName('beat');
+	
+if(sharedBeat.length > 0)
+	$("#tracker").html(LZString.decompressFromBase64(sharedBeat));
 	
 $.fn.toggleDisabled = function() {
 	return this.each(function() {
@@ -45,9 +68,46 @@ $.fn.toggleDisabled = function() {
 	});
 };
 	
+function showShare(){
+	
+	var compressTrack = LZString.compressToBase64($("#tracker").val());
+	var trackUrl = "https://eypacha.github.io/bbx-tracker?beat=" + compressTrack;
+	
+    if ($("#shortlink").val() == ""){
+		
+		get_short_url(trackUrl, function(short_url) {
+
+		if (typeof short_url === 'undefined') {short_url = trackUrl;}
+			$("#fbShare").attr("href","https://www.facebook.com/sharer/sharer.php?u="+short_url);
+			$("#twShare").attr("href","http://twitter.com/share?url="+short_url+"&text=Listen%20to%20this%20beat%20I%20just%20made!&hashtags=bbxtracker,beatbox,bbx");
+			$("#btEmail").attr("href","mailto:?subject=Listen%20to%20this%20beat%20I%20just%20made!&body=Beat%20track%20made%20it%20with%20BBX%20Tracker! > "+short_url);
+			$("#shortlink").val(short_url);
+			$("#shareBtn").addClass("collapse");
+			$("#shareIt").removeClass("collapse");
+		});
+
+    }
+}
+	
+function get_short_url(long_url, func){
+    $.getJSON(
+        "http://api.bitly.com/v3/shorten?callback=?", 
+        { 
+            "format": "json",
+            "apiKey": "R_da6cb00ea3395240c2a5ee7b5b6b1384",
+            "login": "eypacha",
+            "longUrl": long_url
+        },
+        function(response)
+        {
+            func(response.data.url);
+            
+        }
+    );
+}
 function playTracker(){
 	
-	tracker = $("#tracker").val();
+	tracker = $("#tracker").val();	
 	
 	// Remplaza saltos de línea por espacios
 	tracker = tracker.replace(/(\r\n|\n|\r)/gm, " ");
@@ -68,13 +128,19 @@ function playTracker(){
 	patron = setInterval(function(){sonar()},3600/bpm*8/steps);
 	
 	$(".tempo button").toggleDisabled();
+	$("#tracker").toggleDisabled();
+	
+	$("#shortlink").val("");
+	$("#shareBtn").removeClass("collapse");
+	$("#shareIt").addClass("collapse");
+	
 	
 }
 	
 function sonar(){
 	sound.play(tracker[step]);
+	$("#pacha div").css("background-image","url('img/pacha-boca-"+tracker[step]+".png')");
 	step++;
-	console.log(step);
 	if(step >= tracker.length){
 		if(!loop){
 			stopTracker();
@@ -87,9 +153,11 @@ function sonar(){
 function stopTracker(){
 	clearInterval(patron);
 	step = 0;
+	$("#pacha div").css("background-image","url('img/pacha-boca---.png')");
 	$("#playBtn").removeClass("collapse");
 	$("#stopBtn").addClass("collapse");
 	$(".tempo button").toggleDisabled();
+	$("#tracker").toggleDisabled();
 }
 	
 function toggleLoop(){
@@ -107,7 +175,7 @@ function changeTempo(howMuch){
 		bpm += howMuch;
 
 	if(bpm < minBpm)
-		bpm = minBpm
+		bpm = minBpm;
 	
 	if(bpm > maxBpm)
 		bpm = maxBpm;
@@ -121,7 +189,7 @@ function changeSteps(howMuch){
 		steps += howMuch;
 
 	if(steps < 1)
-		steps = 1
+		steps = 1;
 	
 	if(steps > maxSteps)
 		steps = maxSteps;
